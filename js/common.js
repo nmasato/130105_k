da6708269657736d638561de9816cfdc22382304
samplenmasato <naganawa@prod-factory.com>
2013-04-21 19:31:42 +0900

    ［初期ファイル］

---
+++ js/common.js
@@ -0,0 +1,71 @@
+//==========================================
+//
+//	HTML5
+//
+//==========================================
+document.createElement('header');
+document.createElement('hgroup');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('nav');
+
+
+
+//==========================================
+//
+//	Page Top
+//
+//==========================================
+$(function(){
+	$("#pagetop a, .gotop a").click(function(){
+	$('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
+		return false;
+	})
+});
+
+
+
+//==========================================
+//
+//	Hover Light
+//
+//==========================================
+$(function(){
+	$('#globalnavi a,#localnavi a,#footerContents a,#footer a,.indexBox,#pageIndex').mouseover(function(){
+		$(this).css({
+			'opacity': '0.7'
+		});
+		$(this).stop(true, false).animate({
+			'opacity': '1'
+		}, 700);
+	});
+});
+
+
+
+//==========================================
+//
+//	Div All Link
+//
+//==========================================
+$(function(){
+	$(".indexBox,#pageIndex").click(function(){
+		window.location=$(this).find("a").attr("href");
+		return false;
+	});
+});
+
+
+
+//==========================================
+//
+//	Icon ON-OFF
+//
+//==========================================
+$(function(){
+	$("img[src*='_off.']").hover(
+		function(){this.src=this.src.replace("_off.","_on.")},
+		function(){this.src=this.src.replace("_on.","_off.")}
+	)
+});
---
+++ js/import.js
@@ -0,0 +1,3 @@
+document.write('<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->');
+document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>');
+document.write('<script src="/js/common.js" type="text/javascript"></script>');
