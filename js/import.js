34293ede74cb2f4b4138c4bf264f04b194ed8c70
MASATO NAGANAWA <naganawa@prod-factory.com>
2013-04-23 15:04:08 +0900

    ［制作］INDEX作成(HTML)

--- js/import.js
+++ js/import.js
@@ -1,3 +1,4 @@
 document.write('<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->');
 document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>');
-document.write('<script src="/js/common.js" type="text/javascript"></script>');
+document.write('<script src="js/jquery.flatheights.js" type="text/javascript"></script>');
+document.write('<script src="js/common.js" type="text/javascript"></script>');
